1112760e9d60cb116a76757df2ff0077b7b1b301
Nat Ryall <nat@ryall>
2008-07-23 13:18:10 +0000

    - Added a new character select concept. - Added the basic layout for CMinimap (incomplete).
    git-svn-id: svn+ssh://supergirl.dreamhost.com/home/sulfate/svn/pikpik@95 d6e08b45-3f4a-0410-be67-99b8f2306d8d

--- Source/Radar.h
+++ Source/Radar.h
@@ -32,3 +32,47 @@ protected:
 };
 
 //##############################################################################
+
+// The minimap element bit flags.
+enum t_MinimapElement
+{
+	MinimapElement_Floor		= XBIT(0),
+	MinimapElement_Walls		= XBIT(1),
+	MinimapElement_Base			= XBIT(2),
+	MinimapElement_Pellets		= XBIT(3),
+	MinimapElement_Powers		= XBIT(4),
+	MinimapElement_Traps		= XBIT(5),
+	MinimapElement_Pacman		= XBIT(6),
+	MinimapElement_Ghost		= XBIT(7),
+	MinimapElement_PacmanStart	= XBIT(8),
+	MinimapElement_Scanline		= XBIT(9),
+};
+
+/*class CMinimap
+{
+public:
+	// Constructor.
+	CMinimap(CMap* pMap); // Create a new texture and initialise.
+
+	// Destructor.
+	virtual ~CMinimap(); // Free the texture.
+
+	// Generate a minimap with the specified t_MinimapElement mask.
+	HTEXTURE Generate(xuint iElementMask);
+	
+	// Get the minimap texture.
+	inline HTEXTURE GetTexture()
+	{
+		return m_hMinimap;
+	}
+
+protected:
+	// The current map we're working with.
+	CMap* m_pMap;
+
+	// The minimap texture.
+	HTEXTURE m_hMinimap;
+	
+	// The scanline offset.
+	xint iScanlineOffset;
+};*/
