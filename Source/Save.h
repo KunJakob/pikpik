28b7055d79349483a51bf095661d424a7fff3180
Nat Ryall <nat@ryall>
2008-08-29 08:27:13 +0000

    - Updated the SaveManager declaration.
    git-svn-id: svn+ssh://supergirl.dreamhost.com/home/sulfate/svn/pikpik@157 d6e08b45-3f4a-0410-be67-99b8f2306d8d

--- Source/Save.h
+++ Source/Save.h
@@ -37,7 +37,7 @@
 //
 //##############################################################################
 
-// Lists.
+
 
 //##############################################################################
 
@@ -57,33 +57,37 @@ public:
 	}
 
 	// Register a boolean key with the save manager.
+	// ~bDefault The default value to use if the key is unset.
 	void RegisterKey(const xchar* pKey, xbool bDefault);
 
 	// Register a integer key with the save manager.
-	void RegisterKey(const xchar* pKey, xint iDefault);
+	// ~iDefault The default value to use if the key is unset.
+	// ~iMinValue The minimum value to allow for the integer.
+	// ~iMaxValue The maximum value to allow for the integer.
+	void RegisterKey(const xchar* pKey, xint iDefault, xint iMinValue = INT_MIN, xint iMaxValue = INT_MAX);
 
 	// Register a floating-point key with the save manager.
-	void RegisterKey(const xchar* pKey, xfloat fDefault);
+	// ~fDefault The default value to use if the key is unset.
+	// ~fMinValue The minimum value to allow for the float.
+	// ~fMaxValue The maximum value to allow for the float.
+	void RegisterKey(const xchar* pKey, xfloat fDefault, xfloat fMinValue = FLT_MAX, xfloat fMaxValue = FLT_MAX);
 
 	// Register a string key with the save manager.
-	void RegisterKey(const xchar* pKey, xstring xDefault);
+	// ~xDefault The default value to use if the key is unset.
+	// ~iMaxLength The maximum string length to read in (includes NULL terminator).
+	void RegisterKey(const xchar* pKey, xstring xDefault, xint iMaxLength = 64);
 
 	// Get a boolean value from the save data.
 	xbool GetBool(const xchar* pKey);
 
 	// Get an integer value from the save data.
-	// ~iMinValue The minimum value to allow for the integer.
-	// ~iMaxValue The maximum value to allow for the integer.
-	xint GetInt(const xchar* pKey, xint iMinValue = INT_MIN, xint iMaxValue = INT_MAX);
+	xint GetInt(const xchar* pKey);
 
 	// Get a floating-point value from the save data.
-	// ~fMinValue The minimum value to allow for the float.
-	// ~fMaxValue The maximum value to allow for the float.
-	xfloat GetFloat(const xchar* pKey, xfloat fMinValue = FLT_MAX, xfloat fMaxValue = FLT_MAX);
+	xfloat GetFloat(const xchar* pKey);
 
 	// Get a string value from the save data.
-	// ~iMaxLength The maximum string length to read in (includes NULL terminator).
-	xstring GetString(const xchar* pKey, xint iMaxLength = 64);
+	xstring GetString(const xchar* pKey);
 
 	// Save all registered values to a file.
 	void Save(const xchar* pFileName);
@@ -92,7 +96,17 @@ public:
 	void Load(const xchar* pFileName);
 
 protected:
+	// Lists.
+	typedef xhash<xstring, xbool> t_BooleanHash;
+	typedef xhash<xstring, xint> t_IntegerHash;
+	typedef xhash<xstring, xfloat> t_FloatHash;
+	typedef xhash<xstring, xstring> t_StringHash;
+
 	// The list of key-value pairs.
+	t_BooleanHash m_lpBooleans;
+	t_IntegerHash m_lpIntegers;
+	t_FloatHash m_lpFloats;
+	t_StringHash m_lpStringHash;
 };
 
 //##############################################################################
