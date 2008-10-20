c77299a23c9d12cd89896ae76ebf53aa93716f9e
Nat Ryall <nat@ryall>
2008-10-20 21:53:08 +0000

    - Added new interface metadata ready for the interface factory functions. - Added some AI documentation.
    git-svn-id: svn+ssh://supergirl.dreamhost.com/home/sulfate/svn/pikpik@178 d6e08b45-3f4a-0410-be67-99b8f2306d8d

--- Project/BuildData.bat
+++ Project/BuildData.bat
@@ -6,6 +6,7 @@ set OUTPATH="..\Bin\Metadata"
 set KEYFILE="..\Crypto\Keys\AES-256-Key.txt"
 
 %METACOMP% %INPATH%\Fonts.mta %OUTPATH%\Fonts.emta %KEYFILE% > nul
+%METACOMP% %INPATH%\Interface.mta %OUTPATH%\Interface.emta %KEYFILE% > nul
 %METACOMP% %INPATH%\Maps.mta %OUTPATH%\Maps.emta %KEYFILE% > nul
 %METACOMP% %INPATH%\Sounds.mta %OUTPATH%\Sounds.emta %KEYFILE% > nul
 %METACOMP% %INPATH%\Sprites.mta %OUTPATH%\Sprites.emta %KEYFILE% > nul
